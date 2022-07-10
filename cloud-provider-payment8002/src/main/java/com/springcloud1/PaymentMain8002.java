e5db6348faedeaf3a8e93cdadc2ca1deedc0169e
Yanx <1832180488@qq.com>
2022-07-10 18:19:51 +0800

    视频23修改

---
+++ cloud-provider-payment8002/src/main/java/com/springcloud1/PaymentMain8002.java
@@ -0,0 +1,17 @@
+package com.springcloud1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Yanx
+ * @Create 2022-07-07-0:19
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class PaymentMain8002 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8002.class);
+    }
+}
